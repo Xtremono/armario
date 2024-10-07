586e23904957f4c19c5d5b1e92988a26341e78b8
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-10-07 18:12:52 -0300

    agregado el order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,33 @@
 class OrdersController < ApplicationController
-  
+  before_action :set_clothe, only: [:new, :create]
+  before_action :authenticate_user!
+  def index
+    @orders = current_user.orders
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @clothe = Clothe.find(params[:clothe_id])
+    @order = @clothe.orders.new(order_params)
+    @order.user = current_user
+    if @order.save
+      @clothe.update(purchased: true)
+      redirect_to my_orders_path, notice: "La orden se ha creado correctamente."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_clothe
+    @clothe = Clothe.find(params[:clothe_id])
+  end
+
+  def order_params
+    params.require(:order).permit(:price)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :clothe
-
-  validates :price, presence: true
-  validates :order_date, presence: true
 end
